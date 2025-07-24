f4e924245e6aa7f9559e34a48e69dcfff410f01a
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2025-07-24 17:32:02 +0500

    search msg

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,21 @@
+"use client"
 import { Search } from 'lucide-react'
 import React from 'react'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip'
 
 function SearchBar() {
   return (
-    <Search className='w-5 h-5 hoverEffect hover:text-shop_light_green  text-lightColor font-semibold'/>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <Search className='w-5 h-5 hoverEffect hover:text-shop_light_green  text-lightColor font-semibold'/>
+        </TooltipTrigger>
+        <TooltipContent>
+          <span className="text-xs font-bold text-gray-400">Temp unavailable</span>
+        </TooltipContent>
+      </Tooltip>
+
+    </TooltipProvider>
   )
 }
 
